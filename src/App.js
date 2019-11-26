7951e7644b7f248effcc2a5688d00ff4467e94fe
Oshalusi John <oshalusijohn@gmail.com>
2019-11-26 17:19:49 +0100

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    value: 0
+  };
+
+  onChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+    console.log(this.state.value);
+  };
+
+  render() {
+    return (
+      <div className='timer'>
+        <div>00:00:00:00</div>
+        <div>
+          <form>
+            <input
+              id='numLabel'
+              value={this.state.value}
+              onChange={this.onChange}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
